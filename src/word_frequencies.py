6b3e3f4ea1e296b96191484325d9d8997f6d7590
modengann <99417989+modengann@users.noreply.github.com>
2022-09-12 21:40:15 -0400

    init

---
+++ src/word_frequencies.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+def word_frequencies(filename):
+    pass
+
+def main():
+    d = word_frequencies("src/alice.txt"))
+
+
+    #if d["creating"] != 3:
+    #    print("Incorrect count for word 'creating'!")
+    #if d["Carroll"] != 3:
+    #    print("Incorrect count for word 'Carroll'!")
+    #if d["sleepy"] != 2:
+    #    print("Incorrect count for word 'sleepy'!")
+    #if d["Rabbit"] != 28:
+    #    print("Incorrect count for word 'Rabbit'!")
+    #
+    #if len(d) != 2424:
+    #    print(f"Wrong number of words in the dictionary, should be 2424 but is {len(d)}")
+    
+
+
+if __name__ == "__main__":
+    main()
